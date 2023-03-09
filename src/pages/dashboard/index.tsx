404f8c9494cd4f45002f23f4934977759360a7b8
deadaf <gahlyanrohit18@gmail.com>
2023-03-09 15:27:13 +0530

    add unique key to GuildCard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -7,13 +7,13 @@ import { GuildCardDesign, GuildMenu } from "../../utils/styles/menu";
 const GuildCard = ({ guild }: { guild: PartialGuild }) => {
   return (
     <GuildCardDesign>
-      {/* <li> */}
-      <Link to={`/dashboard/${guild.id}`} style={{ textDecoration: "none" }}>
-        <img src={`https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.png`} alt={guild.name} />
-        <span>{guild.name}</span>
-        <button>Manage</button>
-      </Link>
-      {/* </li> */}
+      <li>
+        <Link to={`/dashboard/${guild.id}`} style={{ textDecoration: "none" }}>
+          <img src={`https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.png`} alt={guild.name} />
+          <span>{guild.name}</span>
+          <button>Manage</button>
+        </Link>
+      </li>
     </GuildCardDesign>
   );
 };
@@ -29,7 +29,7 @@ export const MenuPage = () => {
       {user ? (
         <ul style={{ listStyle: "none", padding: 0 }}>
           {user.guilds.map((guild: PartialGuild) => (
-            <GuildCard guild={guild} />
+            <GuildCard guild={guild} key={guild.id} />
           ))}
         </ul>
       ) : (
